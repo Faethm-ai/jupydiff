48286113e71c10c37463f01624c5ceb7b9b67976
MikhailTH <67678986+MikhailTH@users.noreply.github.com>
2020-09-07 10:20:50 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-const fs = require('fs');
+const { inspect } = require("util");
+const core = require("@actions/core");
+const github = require("@actions/github");
+
 function getSha() {
-  if (context.eventName && context.eventName == "pull_request") {
-    return context.payload.pull_request.head.sha;
+  if (github.context.eventName == "pull_request") {
+    return github.context.payload.pull_request.head.sha;
   } else {
-    return context.sha;
+    return github.context.sha;
   }
 }
-async function run(data) {
+
+async function run() {
   try {
-    const sha = getSha();
-    await github.repos.createCommitComment({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
+    const inputs = {
+      token: core.getInput("token"),
+      repository: core.getInput("repository"),
+      sha: core.getInput("sha"),
+      body: core.getInput("body"),
+      path: core.getInput("path"),
+      position: core.getInput("position"),
+    };
+    core.debug(`Inputs: ${inspect(inputs)}`);
+
+    const [owner, repo] = inputs.repository.split("/");
+
+    const sha = inputs.sha ? inputs.sha : getSha();
+    core.debug(`SHA: ${sha}`);
+
+    const octokit = github.getOctokit(inputs.token);
+
+    await octokit.repos.createCommitComment({
+      owner: owner,
+      repo: repo,
       commit_sha: sha,
-      body: data.length == 0 ? "No Changes" : "```diff \n" + data + " ```"
+      body: inputs.body,
+      path: inputs.path,
+      position: inputs.position
     });
   } catch (error) {
+    core.debug(inspect(error));
     core.setFailed(error.message);
   }
-}  
-await fs.readFile('output.txt', function read(err, data) {
-   if (err) {
-     throw err;
-   }
-   run(data.toString());
-});
+}
+
+run();
