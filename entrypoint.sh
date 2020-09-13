f557574489b5e1650a69dc802f04485963e2342a
Mikhail Thornhill <67678986+MikhailTH@users.noreply.github.com>
2020-09-13 22:54:57 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ for f in *.ipynb;
 do nbdime diff --no-color -s $f >> output.txt;
 done
 
-PATH=$PATH:$HOME/.local/bin
+PATH=$PATH:/usr/local/lib/python3.7/site-packages
 rhino blairhudson/jupydiff/comment.js
