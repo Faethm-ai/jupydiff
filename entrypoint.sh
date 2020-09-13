c4045ecccfca9a767152baa54dfccba86a6c9628
Mikhail Thornhill <67678986+MikhailTH@users.noreply.github.com>
2020-09-13 23:05:08 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ do nbdime diff --no-color -s $f >> output.txt;
 done
 
 PATH=$PATH:/usr/local/lib/python3.7/site-packages
-ls /usr/local/lib/python3.7/site-packages
+ls -l /usr/local/lib/python3.7/site-packages
 rhino blairhudson/jupydiff/comment.js
