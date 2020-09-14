569a1ba7cae5bb1deab663893a75597712b4dfec
Mikhail Thornhill <67678986+MikhailTH@users.noreply.github.com>
2020-09-14 14:20:02 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 for f in *.ipynb; 
 do nbdime diff --no-color -s $f >> output.txt;
-cat output.txt;
 done
 
 PATH=$PATH:/usr/local/lib/python3.7/site-packages
-echo $PATH
-cd blairchudson
-pwd
-ls 
-find / -name comment.js
 node /comment.js
