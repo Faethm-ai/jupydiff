7b3016d9ef9a930d1d796353dc5621c057a7e5d0
Mikhail Thornhill <67678986+MikhailTH@users.noreply.github.com>
2020-09-14 11:27:34 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ done
 PATH=$PATH:/root/.cache/pip/wheels/35/cf/c8/564c0e0472dda3a0d198b9031a05a84bcc1a4eadd9c3e3298a
 /usr/local/lib/python3.7/site-packages/rhino
 ls -l 
-rhino blairhudson/jupydiff/comment.js
+node blairhudson/jupydiff/comment.js
