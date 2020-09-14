257e5053ac055b2fbef236210e618e204683b940
Mikhail Thornhill <67678986+MikhailTH@users.noreply.github.com>
2020-09-14 11:31:00 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ do nbdime diff --no-color -s $f >> output.txt;
 cat output.txt;
 done
 
-PATH=$PATH:/root/.cache/pip/wheels/35/cf/c8/564c0e0472dda3a0d198b9031a05a84bcc1a4eadd9c3e3298a
-/usr/local/lib/python3.7/site-packages/rhino
-ls -l 
+PATH=$PATH:/home/runner/work/_actions/blairhudson/jupydiff/master
+
 node blairhudson/jupydiff/comment.js
