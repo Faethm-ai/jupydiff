4e01329bca46674ed86945d344e6fcd2f4891e88
Mikhail Thornhill <67678986+MikhailTH@users.noreply.github.com>
2020-09-14 11:41:59 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ do nbdime diff --no-color -s $f >> output.txt;
 cat output.txt;
 done
 
-PATH=$PATH:/home/runner/work/_actions/blairhudson/jupydiff/master
+PATH=$PATH:/usr/local/lib/python3.7/site-packages
 apt-get -y install nodejs
 node ./comment.js
