199c0f3204b216a3745fdf7c9cf78a6d0f92b7f7
Mikhail Thornhill <67678986+MikhailTH@users.noreply.github.com>
2020-09-14 11:11:52 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ cat output.txt;
 done
 
 PATH=$PATH:/usr/local/lib/python3.7/site-packages
-ls -f /usr/local/lib/python3.7/site-packages/rhino
+ls -lR /usr/local/lib/python3.7/site-packages/rhino
 rhino blairhudson/jupydiff/comment.js
