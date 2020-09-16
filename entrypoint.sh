33277700f4c3f562a210b262284196bfb7433ef4
Mikhail Thornhill <67678986+MikhailTH@users.noreply.github.com>
2020-09-16 16:39:19 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # generate diffs
-git reset --soft HEAD^
+git reset --soft HEAD~
 for f in $(find . -name '*.ipynb' -print); 
 do nbdime diff --no-color -s $f >> output.txt;
 cat output.txt;
