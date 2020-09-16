5102ce9a2c7f3257a077460a7051722ce7cf18d8
Mikhail Thornhill <67678986+MikhailTH@users.noreply.github.com>
2020-09-16 14:13:29 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 
 # generate diffs
 # TODO: make sure this can find .ipynb in subdirectories
+git reset --soft HEAD~1
 for f in $(find . -name '*.ipynb' -print); 
-do nbdime diff --no-color -s $f >> output.txt;
+do echo $f; nbdime diff --no-color -s $f >> output.txt;
 done
 ls -l
 # write a comment to github
