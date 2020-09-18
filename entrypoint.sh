90307eb48576f2b4d50874658ff462be0aca9b64
Mikhail Thornhill <67678986+MikhailTH@users.noreply.github.com>
2020-09-18 11:48:52 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd .
 # generate diffs
-git reset --soft HEAD@{1};
+# git reset --soft HEAD@{1}; 
+git log
 for f in $(find . -name '*.ipynb' -print); 
 do nbdiff --no-color -s $f >> output.txt;
 nbdiff --no-color -s $f
@@ -9,6 +10,7 @@ echo $f;
 cat output.txt;
 done
 ls -lh
+
 
 # write a comment to github
 cd / && npm run
