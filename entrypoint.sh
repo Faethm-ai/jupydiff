a02abdb6b7818b93c0b700148c6a59f5316b6e97
Mikhail Thornhill <67678986+MikhailTH@users.noreply.github.com>
2020-09-18 14:46:40 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,9 @@ cd .
 # generate diffs
 set -x
  
-
-
-nbdime diff HEAD^ HEAD >> output.txt
-
-
-
-
+nbdime diff --no-color -s HEAD^ HEAD >> output.txt
 ls -lh
-
+cat output.txt
 
 # write a comment to github
 cd / && npm run
