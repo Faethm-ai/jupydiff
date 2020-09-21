75143a3e79517d56e9411eceb9f930d93c480fd0
Mikhail Thornhill <67678986+MikhailTH@users.noreply.github.com>
2020-09-21 15:13:44 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 # generate diffs
-# cd pwd
- 
 nbdime diff --no-color -s HEAD^ HEAD >> output.txt
 #mv output.txt /usr/local/lib/python3.8/site-packages/
 
 # write a comment to github
 #npm run
-#cd /usr/local/lib/python3.8/site-packages/
 node /comment.js
