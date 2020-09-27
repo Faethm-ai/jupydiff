2350682933db81439d216ed61c15f3d40ecdca05
Mikhail Thornhill <67678986+MikhailTH@users.noreply.github.com>
2020-09-27 23:31:41 +1000

    Update comment.js

--- comment.js
+++ comment.js
@@ -12,27 +12,22 @@ function getSha() {
 }
 
 async function run(data) {
-  try {
-    const inputs = {
-      token: core.getInput("token"),
-      repository: core.getInput("repository"),
-      sha: core.getInput("sha"),
-      body: core.getInput("body"),
-    };
-
-    const [owner, repo] = inputs.repository.split("/");
-    const sha = inputs.sha ? inputs.sha : getSha();
-   
-
-    const octokit = github.getOctokit(inputs.token);
-
-    await octokit.repos.createCommitComment({
-      owner: owner,
-      repo: repo,
-      commit_sha: sha,
-      body: data.length == 0 ? "No Changes" : "```diff \n" + data + "\n ```"
-    });
-  } 
+  const inputs = {
+    token: core.getInput("token"),
+    repository: core.getInput("repository"),
+    sha: core.getInput("sha"),
+    body: core.getInput("body"),
+  };
+  const [owner, repo] = inputs.repository.split("/");
+  const sha = inputs.sha ? inputs.sha : getSha();
+  const octokit = github.getOctokit(inputs.token);
+  
+  await octokit.repos.createCommitComment({
+    owner: owner,
+    repo: repo,
+    commit_sha: sha,
+    body: data.length == 0 ? "No Changes" : "```diff \n" + data + "\n ```"
+  }); 
 }
 
 fs.readFile('output.txt', function read(err, data) {
