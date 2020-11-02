f653516d2daf78060182cea9eefd45339e5613dd
Mikhail Thornhill <67678986+MikhailTH@users.noreply.github.com>
2020-11-02 13:16:24 +1100

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # generate diffs and save output.txt in root dir
-nbdime diff --no-color -s HEAD^ HEAD >> /output.txt
+nbdime diff --no-color -s HEAD^ HEAD >> /output/output.txt
 
 # write a comment to github (run comment.js)
-cd / && npm run start
+cd /output/ && npm run start
